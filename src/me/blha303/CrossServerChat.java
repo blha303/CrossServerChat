bb94959b89e3fdbacfd9da8b0bf70bf564698a03
Steven Smith <stevensmith.ome@gmail.com>
2013-02-08 15:53:26 +0800

    .

--- src/me/blha303/CrossServerChat.java
+++ src/me/blha303/CrossServerChat.java
@@ -1,5 +1,8 @@
 package me.blha303;
 
+import java.io.IOException;
+
+import com.dwarfscraft.config.BungeeConfig;
 import com.google.common.eventbus.Subscribe;
 
 import net.md_5.bungee.api.ChatColor;
@@ -10,10 +13,18 @@ import net.md_5.bungee.api.plugin.Listener;
 import net.md_5.bungee.api.plugin.Plugin;
 
 public class CrossServerChat extends Plugin implements Listener {
+	
+	BungeeConfig config;
 
 	@Override
 	public void onEnable() {
 		ProxyServer.getInstance().getPluginManager().registerListener(this);
+		try {
+			config = new BungeeConfig(this, "");
+			config.set("string", "&7<&2%s&8-&2%s&7> &f%s");
+		} catch (IOException e) {
+			System.out.println("Could not create CrossServerChat config. Maybe you're out of disk space.");
+		}
 	}
 
 	@Subscribe

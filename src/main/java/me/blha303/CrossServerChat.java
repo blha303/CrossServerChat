0201e054c0b64282ad149d7b0e3c54d78dbcd4b5
Steven Smith <stevensmith.ome@gmail.com>
2013-02-11 05:39:33 +0800

    remove dependency on bungeeconfig

--- src/main/java/me/blha303/CrossServerChat.java
+++ src/main/java/me/blha303/CrossServerChat.java
@@ -1,8 +1,15 @@
 package me.blha303;
 
+import java.io.File;
+import java.io.FileReader;
+import java.io.FileWriter;
 import java.io.IOException;
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.logging.Level;
 
-import com.dwarfscraft.config.BungeeConfig;
+import org.yaml.snakeyaml.Yaml;
+
 import com.google.common.eventbus.Subscribe;
 
 import net.md_5.bungee.api.ChatColor;
@@ -13,35 +20,119 @@ import net.md_5.bungee.api.plugin.Listener;
 import net.md_5.bungee.api.plugin.Plugin;
 
 public class CrossServerChat extends Plugin implements Listener {
-	
-	BungeeConfig config;
+
+	private Yaml yaml;
+	private File file;
+	private Map<String, String> data;
 
 	@Override
 	public void onEnable() {
 		String defaultmsg = "&7<&2%s&8-&2%s&7> &f%s";
-		ProxyServer.getInstance().getPluginManager().registerListener(this);
-		try {
-			config = new BungeeConfig(this, "%s is replaced with (in order) server name, player display name, message. Make sure you have three %s's.");
-			if (config.isNew()) {
-				config.set("string", defaultmsg);
-			}
-		} catch (IOException e) {
-			System.out.println("Could not create CrossServerChat config. Maybe you're out of disk space.");
+		file = new File("plugins" + File.pathSeparator
+				+ this.getDescription().getName() + File.pathSeparator
+				+ "locations.yml");
+		yaml = new Yaml();
+		loadYAML();
+		if (Integer.parseInt(data.get("version")) < Integer.parseInt(this
+				.getDescription().getVersion())) {
+			String a = get("string");
+			if (get("string") == defaultmsg)
+				data.clear();
+			put("version", this.getDescription().getVersion());
+			put("help",
+					"%s is replaced with (in order) server name, player display name, message. Make sure you have all three.");
+			if (a != defaultmsg)
+				put("string", a);
+			put("server.lobby", "Lobby");
 		}
+		ProxyServer.getInstance().getPluginManager().registerListener(this);
 	}
 
 	@Subscribe
 	public void onChat(ChatEvent e) {
 		String m = e.getMessage();
-		String msg = ChatColor.translateAlternateColorCodes('&', config.getString("string"));
+		String servername = "";
+		String msg = ChatColor.translateAlternateColorCodes('&',
+				data.get("string"));
 		if (e.getSender() instanceof ProxiedPlayer) {
 			ProxiedPlayer pl = (ProxiedPlayer) e.getSender();
+			if (containsKey("server."
+					+ pl.getServer().getInfo().getName().toLowerCase())) {
+				servername = ChatColor.translateAlternateColorCodes('&',
+						get("server."
+								+ pl.getServer().getInfo().getName()
+										.toLowerCase()));
+			} else {
+				servername = pl.getServer().getInfo().getName().toLowerCase();
+			}
 			for (ProxiedPlayer p : ProxyServer.getInstance().getPlayers()) {
-				if (p.getServer().getInfo().getName() != pl.getServer().getInfo().getName()) {
-					p.sendMessage(String.format(msg, pl.getServer().getInfo().getName(), pl.getDisplayName(), m));
+				if (p.getServer().getInfo().getName() != pl.getServer()
+						.getInfo().getName()) {
+					p.sendMessage(String.format(msg, servername,
+							pl.getDisplayName(), m));
 				}
 			}
 		}
 	}
 
+	/**
+	 * Initialize YAML
+	 */
+	@SuppressWarnings("unchecked")
+	public void loadYAML() {
+        try {
+            file.createNewFile();
+            try {
+            	FileReader rd = new FileReader(file);
+                data = yaml.loadAs(rd, Map.class);
+            } catch (IOException ex) {
+            ProxyServer.getInstance().getLogger().log(Level.WARNING, "Could not load CrossServerChat config", ex);
+            }
+
+        if (data == null) {
+            data = new ConcurrentHashMap<>();
+        } else {
+            data = new ConcurrentHashMap<>(data);
+        }
+	}
+
+	/**
+	 * Save YAML to file
+	 */
+	public void saveYAML() {
+		try {
+			FileWriter wr = new FileWriter(file);
+			yaml.dump(data, wr);
+		} catch (IOException ex) {
+			ProxyServer.getInstance().getLogger().log(Level.WARNING, "Could not load CrossServerChat config", ex);
+		}
+	}
+
+	/**
+	 * Get a key value from YAML
+	 * 
+	 * @param key Key name
+	 * @return Value of key
+	 */
+	public String get(String key) {
+		return data.get(key);
+	}
+
+	/**
+	 * Put a key value into YAML
+	 * 
+	 * @param key Key name
+	 * @param value Key value
+	 */
+	public void put(String key, String value) {
+		data.put(key, value);
+	}
+
+	public boolean containsKey(String key) {
+		return data.containsKey(key);
+	}
+
+	public boolean containsValue(String value) {
+		return data.containsValue(value);
+	}
 }
